579a04d9151a4b50b2ddd9afefcb9138343b9d74
satilpereira <satil.prog@gmail.com>
2022-09-26 03:34:53 -0300

    components part 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent'
+import './App.scss';
+// import { Component } from 'react';
+
+// class App extends Component() {
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <p>
+//             Edit <code>src/App.js</code> and save to reload.
+//           </p>
+//           <a
+//             className="App-link"
+//             href="https://reactjs.org"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             Learn React
+//           </a>
+//         </header>
+//       </div>
+//     );
+//   }
+// }
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+        <Navbar dark color="primary">
+          <div className='container'>
+            <NavbarBrand href='/'>Ristorant Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu />
+      </div>
   );
 }
 
